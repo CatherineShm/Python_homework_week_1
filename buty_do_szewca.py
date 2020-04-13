b7cb7b0c7509f6cfac9795f2a57c7dc752d7f560
CatherineShm <katerynashmaliukh@gmail.com>
2020-04-13 15:24:33 +0200

    zadanie_1.2

--- buty_do_szewca.py
+++ buty_do_szewca.py
@@ -3,5 +3,18 @@
 # (poniedziałek to dzień 1, wtorek to dzień 2 itp.). Ma też podać, ile dni będzie trwała naprawa.
 # Program ma wypisać, w jaki dzień tygodnia buty będą gotowe do odbioru. Jeśli tak będzie ci wygodniej,
 # możesz założyć, że naprawa butów nigdy nie będzie trwała dłużej niż siedem dni.
+
 dni = ("poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela")
-print(dni)
+
+for x, y in enumerate(dni):
+    print(y,"-", x, end="|")
+
+oddanie = int(input("\nPodaj numer dnia tygodnia, w którym oddałeś buty do naprawy(wg listy powyżej): "))
+dni_naprawy = int(input("Ile dni będzie trwała naprawa: "))
+naprawa = int(oddanie + dni_naprawy)
+
+if naprawa >= 7:
+    naprawa = naprawa % 7
+
+odbior = dni[naprawa]
+print(f"Dzień odbioru butów to: {odbior}.")
